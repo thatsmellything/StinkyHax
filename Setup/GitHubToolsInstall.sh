7bfca5f4a56ad22e38720c1343ef4c1191320213
thatsmellything <thesmellliestman@gmail.com>
2020-04-23 10:30:46 -0600

    Added W-Fi-Pumpkin to tools to be installed

--- Setup/GitHubToolsInstall.sh
+++ Setup/GitHubToolsInstall.sh
@@ -6,4 +6,11 @@ cd githubtools
 echo "Installing thatsmellythings tools"
 sudo git clone https://github.com/thatsmellything/StinkyHax.git
 sudo git clone https://github.com/thatsmellything/JaVuls.git
+echo "Installing WiFi-Pumpkin with GUI"
+git clone https://github.com/P0cL4bs/WiFi-Pumpkin-deprecated.git
+cd WiFi-Pumpkin-deprecated
+sudo chmod +x installer.sh
+echo "dont leave just yet you will need to answer some questions"
+./installer.sh --install
+
 
